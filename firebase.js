f170552dc34837fffd3dd0c0a6461cb05fec2128
AlbertoWindows <albertopimentel04@gmail.com>
2021-01-25 00:08:10 -0600

    inicio proyecto

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYAyX6-wIbSATRnbBS5fPGltkVryQiHFo",
+  authDomain: "signal-clone-rn-5a07b.firebaseapp.com",
+  projectId: "signal-clone-rn-5a07b",
+  storageBucket: "signal-clone-rn-5a07b.appspot.com",
+  messagingSenderId: "508092442995",
+  appId: "1:508092442995:web:ea992a90ba15ad1c557d06",
+};
